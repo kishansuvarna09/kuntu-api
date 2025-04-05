fa9e2549b4ede1e16a0ea5d4d32014a6f42b202b
Kishan Suvarna <kishansuvarna09@gmail.com>
2025-04-05 10:43:40 +0400

    feat: added cors support

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import swagger from "@elysiajs/swagger";
 import { Elysia } from "elysia";
 import postsRoutes from "./routes/posts";
 import userRoutes from "./routes/users";
+import cors from "@elysiajs/cors";
 
 const app = new Elysia();
 
 app
+  .use(cors())
 	.use(swagger())
 	.group("/api", (app) => app.use(postsRoutes).use(userRoutes))
 	.listen(process.env.PORT || 5000);
